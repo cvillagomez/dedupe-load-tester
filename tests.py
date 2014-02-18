4b8d019eb16741b9bb422c16c99699b7bb8b5d2c
Eric van Zanten <eric.vanzanten@gmail.com>
2014-02-18 17:29:26 -0600

    Added a bunch more loggin

--- tests.py
+++ tests.py
@@ -1,7 +1,6 @@
-import os
-import time
 import requests
 from multiprocessing import Pool
+import time
 
 def runit(url):
     s = requests.Session()
@@ -14,27 +13,43 @@ def runit(url):
     print 'Select fields took %s seconds' % (str(end - start))
     yes = 0
     no = 0
+    timeout = 15
+    resp = s.get('%s/get-pair/' % url)
+    print resp
     for i in range(12):
-        s.get('%s/mark-pair/' % url, params={'action': 'yes'})
-        s.get('%s/get-pair/' % url)
-        yes += 1
-        print 'Training: %s yes %s no' % (yes, no)
-        time.sleep(2)
+        try:
+            s.get('%s/mark-pair/' % url, params={'action': 'yes'}, timeout=timeout)
+            s.get('%s/get-pair/' % url, timeout=timeout)
+            yes += 1
+            print 'Training: %s yes %s no' % (yes, no)
+            time.sleep(2)
+        except requests.exceptions.Timeout:
+            return 'Marking pair #%s failed' % yes
     for i in range(12):
-        s.get('%s/mark-pair/' % url, params={'action': 'no'})
-        s.get('%s/get-pair/' % url)
-        no += 1
-        print 'Training: %s yes %s no' % (yes, no)
-        time.sleep(2)
-    s.get('%s/mark-pair/' % url, params={'action': 'finish'})
-    start = time.time()
-    while True:
-        if s.get('%s/working/' % url).json().get('ready'):
-            end = time.time()
-            break
-        else:
-            time.sleep(3)
-            continue
+        try:
+            s.get('%s/mark-pair/' % url, params={'action': 'no'}, timeout=timeout)
+            s.get('%s/get-pair/' % url, timeout=timeout)
+            no += 1
+            print 'Training: %s yes %s no' % (yes, no)
+            time.sleep(2)
+        except requests.exceptions.Timeout:
+            return 'Marking pair #%s failed' % (yes + no)
+    try:
+        s.get('%s/mark-pair/' % url, params={'action': 'finish'}, timeout=timeout)
+        start = time.time()
+        print 'Deduplication started...'
+    except requests.exceptions.Timeout:
+        return 'Training finish failed'
+    try:
+        while True:
+            if s.get('%s/working/' % url, timeout=timeout).json().get('ready'):
+                end = time.time()
+                break
+            else:
+                time.sleep(3)
+                continue
+    except requests.exceptions.Timeout:
+        return 'Failed while waiting for results'
     print 'Dedupe Took %s seconds' % (str(end - start))
     return None
 
@@ -42,8 +57,8 @@ if __name__ == '__main__':
     import sys
     count = int(sys.argv[1])
     pool = Pool(processes=count)
-    args = 'http://dedupe.datamade.us'
+    args = 'http://127.0.0.1:9999'
     args_map = []
     for i in range(count):
         args_map.append(args)
-    pool.map(runit, args_map)
+    print pool.map(runit, args_map)
